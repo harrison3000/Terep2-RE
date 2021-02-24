7e982bb5168a5327a9d4e9518604a5f1e3fff9fc
harrison3000 <harrison3000@somewhere>
2021-02-24 18:55:22 -0300

    reimplement a function that was a bunch of repeted instructions to use loops
    so ghidra stops complaining

--- runterep.asm
+++ runterep.asm
@@ -2,7 +2,42 @@ bits 16
 
 segment code align=16
 	;must be 23760 (0x5CD0) bytes,
-	incbin "code.bin"
+	incbin "code.bin",0,0x58fc
+
+	;reimplementation of the function at 0x58fc
+	sbr: ; Start of a Bunch of Repeated instructions
+	push ax
+	xchg ah,al
+	mov dx, 0x388
+	out dx, al
+
+	push ecx
+
+	mov ecx, 6
+	lab1:
+	in al,dx
+	loop lab1
+
+	inc dx		;0x5909
+	mov al,ah	;0x590a
+	out dx,al	;0x590c
+
+	mov ecx, 28
+	lab2:
+	in al,dx
+	loop lab2
+
+	;0x5929
+	dec dx
+	pop ecx
+	pop ax
+	ret
+
+	;0x30 is the size of the original function
+	times (0x30 - ($ - sbr)) nop ;pad with nops
+
+	incbin "code.bin",$
+
 
 segment data align=16
 	;I tried 62224 (0xF310) bytes and it worked
