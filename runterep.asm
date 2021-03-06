101073548ff8c6c064bcf7a0b3adfa4dacba83e2
harrison3000 <harrison3000@somewhere>
2021-03-06 19:55:44 -0300

    get rid of the code manually setting up the enviroment

--- runterep.asm
+++ runterep.asm
@@ -4,6 +4,8 @@ bits 16
 %include "macros.asm"
 
 segment code align=16
+	..start:
+
 	patchPoint 0x52
 	mov ax, data
 
@@ -21,37 +23,3 @@ segment stack class=stack
 	;originally it was 0x400  (0x80  * 8)
 	;now its           0x2000 (0x400 * 8)
 	times 0x400 db "StackSeg"
-
-segment code2
-	resb 0x100 ;just to separate things
-
-	..start:
-	mov ax, 0x2000
-	mov sp, ax
-
-	;I don't know what this magic number is, but the original code did this
-	mov ax, 0xdfd8
-	mov bp, ax
-
-	;register clearing
-	xor eax, eax
-	xor ebx, ebx
-	xor ecx, ecx
-	xor edx, edx
-
-	xor esi, esi
-	xor edi, edi
-	;mov edi, 00270000h
-
-	mov fs, ax
-	mov gs, ax
-
-
-	call code:0000h
-
-	nop
-
-	;DOS "terminate" function and return, this will never be reached, but keeps ghidra happy
-	mov ah,0x4C
-	int 0x21
-	ret
